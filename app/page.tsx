3d2e949a7434fe3c0355fe5c59ac210c9782e885
RITESH RAJ AS <riteshrajas.arulselvan@gmail.com>
2024-08-14 01:51:30 +0000

    Ritesh is the best programmer

--- app/page.tsx
+++ app/page.tsx
@@ -16,11 +16,18 @@ export default function Home() {
             <p>Home to all FEDS</p>
           </div>
         </a>
-        <a href="" className="linktree-link">
+        <a className="linktree-link">
           <div className="linktree-link-content">
-            <img src="https://via.placeholder.com/150" alt="Link 2" />
+          <iframe src="https://drive.google.com/file/d/1qJZPIv3xNWZR52-j2sTAv-UFIQAihI9y/preview" allowTransparency title='2924 Impact Video' allowFullScreen width="auto" height="auto" allow="autoplay"></iframe>
             <h3>Impact Video</h3>
             <p>Year 2023-24 Impact Video</p>
+          </div>
+        </a>
+        <a href="https://feds-handbook.vercel.app/" className="linktree-link">
+          <div className="linktree-link-content">
+            <img src="https://i.imgur.com/JcSYyz7.jpg" alt="Link 3" />
+            <h3>Technical Documentation</h3>
+            <p>Documents for all incoming FEDS</p>
           </div>
         </a>
         <a href="https://example.com" className="linktree-link">
